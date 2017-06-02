e1a4a696b9702baaece7de1beb5afd642b0da8c8
Sai krishna <[saich94@gmail.com]>
2017-06-02 11:49:16 +0530

    sai

---
+++ src/TestPackage/Arithmetic.java
@@ -0,0 +1,43 @@
+package TestPackage;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import javax.ws.rs.PathParam;
+
+@Path("/")
+
+public class Arithmetic
+{
+  @GET
+  @Path("sum/{a}/{b}")
+
+  @Produces({"application/json"})
+  public JSONObject sum(@PathParam("a") double a, @PathParam("b") double b) throws JSONException
+   
+  {
+	    JSONObject j = new JSONObject();
+    double sum = a + b;
+    j.put("Sum is", sum);
+    return j;
+  }
+  
+  @GET
+  @Path("/subtraction/{a}/{b}")
+
+  @Produces({"application/json"})
+  public JSONObject subtraction(@PathParam("a") double a, @PathParam("b") double b) throws JSONException
+   
+  {
+
+	    JSONObject j = new JSONObject();
+  double subtraction = a - b;
+  j.put("Subtraction is", subtraction);
+  return j;
+  }
+  
+}
